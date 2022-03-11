378d9ebf9292ecc741bb3c9a41a1cd6535af3c7a
Dhruv Aggarwal <aggarwaldhruv1993@gmail.com>
2022-03-11 11:02:53 +0530

    HarshiCorp vault copy

---
+++ vault_copy.py
@@ -0,0 +1,56 @@
+import subprocess
+import json
+import os
+
+
+class VaultCopy:
+    """
+    Copy vault from one path to another recursively
+    """
+
+
+    def get_secret_formatted(self, _path):
+        os.environ['VAULT_ADDR'] = 
+        os.environ["VAULT_TOKEN"] = 
+        output = subprocess.run(["vault", "kv", "get", "-format=json", _path], stdout=subprocess.PIPE).stdout.decode("utf-8")
+        kv=json.loads(output) if output else {}
+        _data = kv.get("data", {}).get("data", {})
+        return _data
+
+    def create_json_file(self, json_data):
+        with open("vault.json", "w") as f:
+            f.write(json.dumps(json_data))
+
+    def push_to_vault(self, output_path):
+        os.environ['VAULT_ADDR'] = 
+        os.environ["VAULT_TOKEN"] = 
+        print("Writing data to {}".format(output_path))
+        subprocess.run(["vault", "kv", "put", "-format=json", output_path, "@vault.json"])
+
+    def list_kv(self, path):
+        output = subprocess.run(["vault", "kv", "list", "-format=json", path], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        return json.loads(output) if output else {}
+
+    def recurse_path(self, input_path, output_path):
+        child_paths = self.list_kv(input_path)
+        if child_paths:
+            for _path in child_paths:
+                suffix = _path+("/" if not _path.endswith("/") else "")
+                self.recurse_path(
+                    input_path+suffix,
+                    output_path+suffix
+                )
+        else:
+            self.process(input_path, output_path)
+
+    def process(self, input_path, output_path):
+        print("Processing: {}".format(input_path))
+        json_data = self.get_secret_formatted(input_path, )
+        self.create_json_file(json_data)
+        self.push_to_vault(output_path)
+
+
+VaultCopy().recurse_path(
+    input_path="user-sor/",
+    output_path="user-sor-sandbox/"
+)
